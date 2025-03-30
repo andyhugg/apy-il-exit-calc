a7f4f71dfa28de0bd15da386d966b925df81484a
andyhugg <andyhugg@proton.me>
2025-03-30 09:50:21 -0400

    Update pool_analyzer_devtest.py

--- pool_analyzer_devtest.py
+++ pool_analyzer_devtest.py
@@ -312,7 +312,7 @@ st.markdown("""
 """, unsafe_allow_html=True)
 
 # Title and Introduction
-st.title("Arta - Know the Price. Master the Risk.")
+st.title("Arta - Master the Risk.")
 st.markdown("""
 Arta - Indonesian for "wealth" - was the name of my cat and now the name of my app! It's perfect for fast, accurate insights into price projections, potential profits, and crypto asset or liquidity pool risk. You can run scenarios, test your assumptions, and sharpen your edge, all in real time. **Builder - AHU**
 """)
